fcb53fcb458e2d6a498e15f341b64319fbbb1fe4
Anujraghuwanshi2408 <128280445+Anujraghuwanshi2408@users.noreply.github.com>
2023-05-11 18:26:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-function threeSum(arr, target) {
-// write your code here
+// function threeSum(arr, target) {
+// // write your code here
+	
   
+// }
+ let s = "";
+function number(n){
+	while(n>0){
+		s += n%2;
+		n = parseInt(n/2);
+	}
+	console.log(s);
 }
-
 module.exports = threeSum;

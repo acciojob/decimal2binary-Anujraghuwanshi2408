a20ef30ea6876deff4316b8bf2bcca41c5b49fbd
Anujraghuwanshi2408 <128280445+Anujraghuwanshi2408@users.noreply.github.com>
2023-06-14 11:56:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,47 @@
-// function threeSum(arr, target) {
-// // write your code here
-	
-  
-// }
- let s = "";
-function number(n){
-	while(n>0){
-		s += n%2;
-		n = parseInt(n/2);
+function threeSum(arr, target) {
+// write your code here
+
+	arr.sort((a , b)=>a-b);
+
+	let n = arr.length;
+	let closestSum = Infinity;
+
+	for(let i=0; i<n-2; i++){
+		let left = i+1;
+		let right = n-1;
+		while(left<right){
+			let sum = arr[i]+arr[left]+arr[right];
+
+			if(Math.abs(target-sum) < Math.abs(target-closestSum)){
+				closestSum = sum;
+			}
+
+			if(sum < target){
+				left++;
+			}
+			else{
+				right--;
+			}
+		}
 	}
-// 	return s;
+		return closestSum;
+  
 }
-  number(n);
+
+module.exports = threeSum;
+//  let s = "";
+// function number(n){
+// 	while(n>0){
+// 		s += n%2;
+// 		n = parseInt(n/2);
+// 	}
+// // 	return s;
+// }
+//   number(n);
   
-  let ans = ""
-	for(let i=s.length-1; i>=0; i--){
-	  ans += s[i];
-	}
-console.log(ans);
+//   let ans = ""
+// 	for(let i=s.length-1; i>=0; i--){
+// 	  ans += s[i];
+// 	}
+// console.log(ans);
 // module.exports = threeSum;
